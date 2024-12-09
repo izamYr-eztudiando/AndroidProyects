eb4f130952fcf8bae91416cb02c9a1d08c9ce169
izamYr-eztudiando <isamirvalencia@centroculturalsalmantino.com>
2024-12-09 23:24:30 +0100

    update Camara.java
    comentarios explicando el código

--- Herramientas v2/app/src/main/java/com/salmantino/herramientas/Camara.java
+++ Herramientas v2/app/src/main/java/com/salmantino/herramientas/Camara.java
@@ -25,31 +25,35 @@ import android.widget.Toast;
 
 public class Camara extends Fragment {
 
-    private static final int REQUEST_CODE = 22;
+    private static final int REQUEST_CODE = 22; // Código constante para identificar la solicitud de permisos y actividad de captura de imagen
     private Button btnCamara;
-    private ImageView imageView;
-    private Context context;
+    private ImageView imageView; // En el Image View se mostrará la imagen capturada
+    private Context context; // Utilizamos el contecto de la aplicación para acceder a recursos y servicios del sistema
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Devolvemos en el objeto context, el contexto del fragmento
         context = requireContext();
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        // Insuflamos
         View viewCamara = inflater.inflate(R.layout.fragment_camara, container, false);
         btnCamara = viewCamara.findViewById(R.id.btncamara);
         imageView = viewCamara.findViewById(R.id.imgvCamara);
-
-        btnCamara.setOnClickListener(new View.OnClickListener() {
+        // Cuando pulsemos el botón se ejecutará el código siguiente
+        btnCamara.setOnClickListener(new View.OnClickListener() { 
             @Override
             public void onClick(View v) {
+                // Se verifica si el permiso de la cámara se ha concedido
                 if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // Si no ha sido concedido, se solicita el permiso
                     ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, REQUEST_CODE);
-                } else {
+                } ele {
+                    // Si se concedió, se crea un Intent para abrir la cámara y se llama a startActivityForResult para iniciar la actividad de captura de imagen
                     Intent camaraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     startActivityForResult(camaraIntent, REQUEST_CODE);
                 }
@@ -62,13 +66,16 @@ public class Camara extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        // Verificacion de que la el código coincide y el resultado es bueno 
         if (requestCode == REQUEST_CODE &&  resultCode == RESULT_OK)
         {
+            // Si la captura fue exitosa, se obtiene el bitmap de la imagen desde los extras del intent de la camara y se pone en el ImageView
             if (data != null && data.getExtras() != null) {
                 Bitmap imgBitmap = (Bitmap) data.getExtras().get("data");
                 imageView.setImageBitmap(imgBitmap);
             }
         }
+            // Sino mensaje de error
         else {
             Toast.makeText(context, "Cancelada", Toast.LENGTH_SHORT).show();
         }
@@ -79,6 +86,7 @@ public class Camara extends Fragment {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_CODE) {
+            // Solicitud de permisos, verificación de que el usuario respondió cediendo los permisos
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Intent camaraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(camaraIntent, REQUEST_CODE);
