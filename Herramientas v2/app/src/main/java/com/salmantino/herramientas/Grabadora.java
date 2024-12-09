b482b223e5d29eaca462cc5b15f9880963d97637
izamYr-eztudiando <isamirvalencia@centroculturalsalmantino.com>
2024-12-09 14:51:02 +0100

    09-12-2024

--- Herramientas v2/app/src/main/java/com/salmantino/herramientas/Grabadora.java
+++ Herramientas v2/app/src/main/java/com/salmantino/herramientas/Grabadora.java
@@ -1,4 +1,5 @@
 package com.salmantino.herramientas;
+import android.Manifest;
 import android.media.MediaRecorder;
 import android.widget.ImageView;
 
@@ -25,87 +26,110 @@ import java.io.IOException;
 
 
 public class Grabadora extends Fragment {
-
-    MediaRecorder grabadora = null;
-    String ruta = null;
-    ImageView imgGrabacion = null;
-
-
-
-    private MediaRecorder grabacion;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private MediaRecorder grabadora = null;
     private String archivoSalida = null;
     private Button btn_recorder;
+    private MediaPlayer mediaPlayer;
+    private String[] permissions = {
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+
+//    private MediaRecorder grabacion;
+//    private String archivoSalida = null;
+//    private Button btn_recorder;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-
     }
-/*
-    public void recorder(View view){
-        if (grabacion == null) {
-            archivoSalida = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Grabacion.3gp";
-            grabacion = new MediaRecorder();
-            grabacion.setAudioSource(MediaRecorder.AudioSource.MIC);
-            grabacion.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            grabacion.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-            grabacion.setOutputFile(archivoSalida);
 
-            try {
-                grabacion.prepare();
-                grabacion.start();
-            } catch (IOException e) {
-                e.getSuppressed();
-            }
+    public void iniciarGrabacion() {
+        archivoSalida = requireActivity().getExternalFilesDir(null).getAbsolutePath() + "/Grabacion.3gp";
+        //archivoSalida = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Grabacion.3gp";
+        grabadora = new MediaRecorder();
+        grabadora.setAudioSource(MediaRecorder.AudioSource.MIC);
+        grabadora.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        grabadora.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        grabadora.setOutputFile(archivoSalida);
 
-            btn_recorder.setBackgroundResource(R.drawable.btn_grabar);
+        try {
+            grabadora.prepare();
+            grabadora.start();
+            btn_recorder.setBackgroundResource(R.drawable.btn_grabar); // Cambia el icono a detener
             Toast.makeText(requireActivity().getApplicationContext(), "Grabando", Toast.LENGTH_SHORT).show();
-        } else {
-            grabacion.stop();
-            grabacion.release();
-            grabacion = null;
-            btn_recorder.setBackgroundResource(R.drawable.btn_grabar);
+        } catch (IOException e){
+            e.printStackTrace();
+            Toast.makeText(requireActivity().getApplicationContext(), "Error al iniciar la grabación: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void detenerGrabacion() {
+        if (grabadora != null) {
+            grabadora.stop();
+            grabadora.release();
+            grabadora = null;
+            btn_recorder.setBackgroundResource(R.drawable.btn_grabar); // Cambia el icono a grabar
             Toast.makeText(requireActivity().getApplicationContext(), "Grabación finalizada", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void reproduccion(View view){
-        MediaPlayer mediaPlayer = new MediaPlayer();
+    private void reproducirAudio() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release(); // Libera el MediaPlayer
+        }
+
+        mediaPlayer = new MediaPlayer();
         try {
             mediaPlayer.setDataSource(archivoSalida);
             mediaPlayer.prepare();
-        } catch (IOException e){
+            mediaPlayer.start();
+            Toast.makeText(requireActivity().getApplicationContext(), "Reproduciendo audio", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
             e.printStackTrace();
+            Toast.makeText(requireActivity().getApplicationContext(), "Error al reproducir el audio", Toast.LENGTH_SHORT).show();
         }
-
-        mediaPlayer.start();
-        Toast.makeText(requireActivity().getApplicationContext(), "Reproduciendo audio", Toast.LENGTH_SHORT).show();
     }
-*/
-    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
+
+    @SuppressLint("WrongViewCast")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        if (ContextCompat.checkSelfPermission(requireActivity().getApplicationContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireActivity().getApplicationContext(), android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(requireActivity(), new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.RECORD_AUDIO}, 1000);
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO_PERMISSION);
         }
+
         // Inflamos el layout del fragmento
         View viewGrabadora = inflater.inflate(R.layout.fragment_grabadora, container, false);
+        btn_recorder = viewGrabadora.findViewById(R.id.btnGrabadora);
 
-        // Ahora puedes encontrar el botón usando la vista inflada
-        btn_recorder = viewGrabadora.findViewById(R.id.grabadora);
-
-        // Aquí puedes configurar el botón, por ejemplo, agregar un listener
-
-      /*  btn_recorder.setOnClickListener(view -> {
-            if (grabacion == null) {
-                recorder(view);
+        btn_recorder.setOnClickListener(view -> {
+            if (grabadora == null) {
+                iniciarGrabacion();
             } else {
-                reproduccion(view);
+                detenerGrabacion();
             }
         });
-*/
+
+        // Botón para reproducir el audio grabado
+        Button btn_reproducir = viewGrabadora.findViewById(R.id.btnReproductora);
+        btn_reproducir.setOnClickListener(view -> reproducirAudio());
+
         return viewGrabadora;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (grabadora != null){
+            grabadora.release();
+            grabadora = null;
+        }
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
--- Herramientas v2/app/src/main/java/com/salmantino/herramientas/Ubicacion.java
+++ Herramientas v2/app/src/main/java/com/salmantino/herramientas/Ubicacion.java
@@ -5,23 +5,37 @@ import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.salmantino.herramientas.R;
 
 public class Ubicacion extends Fragment {
     private FusedLocationProviderClient fusedLocationProviderClient;
     private TextView locationTv;
     private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
+    private Button btnUbicacion;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+    }
+
+    @SuppressLint("MissingPermission")
     private void getCurrentLocation() {
         if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -29,56 +43,58 @@ public class Ubicacion extends Fragment {
             return;
         }
 
-        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(location -> {
-            if (location != null) {
-                locationTv.setText("Latitud: " + location.getLatitude() + "\n" + "Longitud: " + location.getLongitude());
-            } else {
-                locationTv.setText("No se pudo obtener la ubicación");
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000); // Intervalo de actualización en milisegundos
+        locationRequest.setFastestInterval(5000); // Intervalo más rápido en milisegundos
+
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    if (location != null) {
+                        locationTv.setText("Latitud: " + location.getLatitude() + "\n" + "Longitud: " + location.getLongitude());
+                    }
+                }
             }
-        });
+        }, Looper.getMainLooper());
+
+//        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(location -> {
+//            if (location != null) {
+//                locationTv.setText("Latitud: " + location.getLatitude() + "\n" + "Longitud: " + location.getLongitude());
+//            } else {
+//                locationTv.setText("No se pudo obtener la ubicación");
+//            }
+//        });
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 getCurrentLocation();
             } else {
-                locationTv.setText("Permiso de ubicación denegado");
+                Toast.makeText(getContext(), "Permiso de ubicación denegado", Toast.LENGTH_SHORT).show();
+                //locationTv.setText("Permiso de ubicación denegado");
             }
         }
     }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
 
     @SuppressLint("WrongViewCast")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_ubicacion, container, false); // Asegúrate de que el layout sea correcto
-        locationTv = view.findViewById(R.id.btn_ubicacion); // Asegúrate de que el ID sea correcto
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        View viewUbicacion = inflater.inflate(R.layout.fragment_ubicacion, container, false);
+        locationTv = viewUbicacion.findViewById(R.id.tv_ubicacion);
+        btnUbicacion = viewUbicacion.findViewById(R.id.btn_actualizar_ubicacion);
+        btnUbicacion.setOnClickListener(view -> getCurrentLocation());
+        //getCurrentLocation();
 
-        getCurrentLocation(); // Llama a la función para obtener la ubicación al crear la vista
-
-        return view;
+        return viewUbicacion;
     }
 
-    /* @SuppressLint("WrongViewCast")
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-
-        View viewUbicacion = inflater.inflate(R.layout.fragment_ubicacion, container, false);
-        locationTv = viewUbicacion.findViewById(R.id.btn_ubicacion);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
-
-        locationTv.setOnClickListener(view -> getCurrentLocation());
-
-        return inflater.inflate(R.layout.fragment_ubicacion, container, false);
-    } */
 }
